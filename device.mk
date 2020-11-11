9e64ed5f12e3b8d8160504784106ddb879b5dd09
mranonymz <mprabhat774@gmail.com>
2020-11-11 10:02:35 +0530

    X00TD: Fix derp

--- device.mk
+++ device.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.common@2.0-util \
-    android.hardware.soundtrigger@2.1-impl \    
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio@6.0 \
     android.hardware.audio.common@6.0 \
     android.hardware.audio.common@6.0-util \
